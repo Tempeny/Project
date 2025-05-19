a8dcce1357f8ee572fe337e982d4c72c3c3a673a
Erik Rodríguez Bernal <104200615+Tempeny@users.noreply.github.com>
2025-05-19 09:32:05 +0200

    Add files via upload

---
+++ Clase2ex5/src/Ex5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ex5 {
+    private static int[] inverseArray(int[] array) {
+        int[] aux = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            aux[i] = array[array.length - i - 1];
+        }
+        return aux;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce el tamaño del array:");
+        int num = scanner.nextInt();
+
+        int[] array = new int[num];
+
+        System.out.println("Introduce " + num + " numeros:");
+        for (int i = 0; i < num; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int[] reversedArray = inverseArray(array);
+
+        System.out.println("El array invertido es:");
+        for (int i = 0; i < reversedArray.length; i++) {
+            System.out.print(reversedArray[i] + " ");
+        }
+        scanner.close();
+    }
+}
